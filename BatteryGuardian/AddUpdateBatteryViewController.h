f885230a76eee4af04878299423634d4db20b0df
Brian Picard <manen19@gmail.com>
2013-03-17 01:23:40 -0400

    Uploading new content

---
+++ BatteryGuardian/AddUpdateBatteryViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddUpdateBatteryViewController.h
+//  BatteryGuardian
+//
+//  Created by brian on 3/17/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddUpdateBatteryViewController : UIViewController
+
+@end
